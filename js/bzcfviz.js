3d995f90acf6f6f617198eb4c861f38d3ac8016b
sandywadhwa <sandy.wadhwa@gmail.com>
2022-10-17 11:34:04 +0530

    Added APIs

--- js/bzcfviz.js
+++ js/bzcfviz.js
@@ -2,19 +2,22 @@ $(function(){
 
 	var cf_api_url_base = 'https://codeforces.com/api/';
 	var cf_api_url_user_info = cf_api_url_base + "user.info";
+	var cf_api_url_user_rating = cf_api_url_base + "user.rating";
+	var cf_api_url_user_status = cf_api_url_base + "user.status";
+	var cf_api_url_contest_standings = cf_api_url_base + "contest.standings";
 
 	var jqxhr = $.ajax( {
-		url: cf_api_url_user_info,
+		url: cf_api_url_user_status,
 		type: "get",
 		data: { 
-			handles: "sushmanth.dampur8780" 
+			handle: "sushmanth.dampur8780" 
 		}
 	} )
 	.done(function(response) {
 		alert( JSON.stringify(response) );
 	})
 	.fail(function(error) {
-		alert( error );
+		alert( JSON.stringify(error) );
 	})
 	// .always(function(xhr) {
 	// 	alert( JSON.stringify(xhr) );
